0c06a0369f3c3ceff9a6ea69ab32d24c403943a6
urvashi29 <urvashigolu29@gmail.com>
2023-02-20 09:55:03 +0530

    initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Hello</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+// Transpiler
+// compiler
+
+
